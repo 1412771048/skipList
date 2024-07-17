eecef8114841477b5087bee5f453038d499f8d04
geyulong <1412771048@qq.com>
2024-07-17 15:13:17 +0800

    xxx

--- main.cpp
+++ main.cpp
@@ -1,50 +1,43 @@
-#include <thread>
-#include <chrono>
-#include <atomic>
-#include <memory>
-#include "skip_list.h"
-
+/* ************************************************************************
+> File Name:     main.cpp
+> Author:        程序员Carl
+> 微信公众号:    代码随想录
+> Created Time:  Sun Dec  2 20:21:41 2018
+> Description:   
+ ************************************************************************/
+#include <iostream>
+#include "skiplist.h"
+#define FILE_PATH "./store/dumpFile"
 
 int main() {
-    //在堆区创建，因为你也不知道跳表有多大呢
-    // skipList::SkipList<int, int> *skip_list = new skipList::SkipList<int, int>();
-    auto skip_list = std::make_unique<skipList::SkipList<int, int>>();
-    for (int i = 0; i < 1000; i++) {
-        skip_list->insert_element(i, i);
-    }
 
-    auto time1= std::chrono::high_resolution_clock::now();
-    skip_list->dump_file();
-    auto time2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1).count();
-    std::cout << "Serialization to disk took " << duration << " milliseconds." << std::endl;
+    // 键值中的key用int型，如果用其他类型，需要自定义比较函数
+    // 而且如果修改key的类型，同时需要修改skipList.load_file函数
+    SkipList<int, std::string> skipList(6);
+	skipList.insert_element(1, "学"); 
+	skipList.insert_element(3, "算法"); 
+	skipList.insert_element(7, "认准"); 
+	skipList.insert_element(8, "微信公众号：代码随想录"); 
+	skipList.insert_element(9, "学习"); 
+	skipList.insert_element(19, "算法不迷路"); 
+	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+
+    std::cout << "skipList size:" << skipList.size() << std::endl;
+
+    skipList.dump_file();
+
+    // skipList.load_file();
+
+    skipList.search_element(9);
+    skipList.search_element(18);
 
 
-    // for (int i = 0, j = 0; i < 5; i++, j++) {
-    //     if (skip_list->insert_element(i, j) == 0) {
-    //         std::cout << "Insert Success" << std::endl;
-    //     } else {
-    //         std::cout << "Insert Failed" << std::endl;
-    //     }
-    // }
+    skipList.display_list();
 
+    skipList.delete_element(3);
+    skipList.delete_element(7);
 
-    // skip_list->display_list();
-    // skip_list->dump_file();
+    std::cout << "skipList size:" << skipList.size() << std::endl;
 
-    // skip_list->load_file();
-
-    // for (int i = 0; i < 3; i++) {
-    //     skip_list->delete_element(i);
-    // }
-
-    // // 搜索
-    // for (int i = 0; i < 5; i++) {
-    //     if (skip_list->search_element(i)) {
-    //         std::cout << "key:" << i << " exist" << std::endl;
-    //     } else {
-    //         std::cout << "key:" << i << " not exist" << std::endl;
-    //     }
-    // }
-    // skip_list->display_list();
+    skipList.display_list();
 }
